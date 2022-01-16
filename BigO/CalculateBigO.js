299eb23192e8a20bb6608155d1464cfc048bf094
Fikcup <rhys.n.wright@gmail.com>
2022-01-15 22:02:10 -0800

    simplify BigO calculation

--- BigO/CalculateBigO.js
+++ BigO/CalculateBigO.js
@@ -12,4 +12,26 @@ function funChallenge(input) {
 }
 
 // O(4n + 3)
-// note: some people do not count assignments or returns towards Big O
+// Simplified to O(n)
+// note: some people do not count assignments, returns, or loops themselves towards Big O
+
+// What is the Big O of the below function? (Hint, you may want to go line by line)
+function anotherFunChallenge(input) {
+    let a = 5; // O(1)
+    let b = 10; // O(1)
+    let c = 50; // O(1)
+    for (let i = 0; i < input; i++) { // O(n)
+        let x = i + 1; // O(n)
+        let y = i + 2; // O(n)
+        let z = i + 3; // O(n)
+
+    }
+    for (let j = 0; j < input; j++) { // O(n)
+        let p = j * 2; // O(n)
+        let q = j * 2; // O(n)
+    }
+    let whoAmI = "I don't know"; // O(1)
+}
+
+// O(7n + 4)
+// Simplified to O(n)
